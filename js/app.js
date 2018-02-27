b04d2b8d5eae532e1a1da6883734e2f52cbbc683
Agata-Czerwinska <agata@rubrica.pl>
2018-02-27 22:36:48 +0100

    Add files
    Implemented: building a grid of cards, shuffle function, flipping cards on click

---
+++ js/app.js
@@ -0,0 +1,159 @@
+function initializeMemoryGame() {
+    addEventListenersToCards();
+}
+/*
+ * Create a list that holds all of your cards
+ */
+const list = document.getElementById('board');
+
+const cards = [
+    {
+        id: 'img-1-1',
+        img: 'img/001-lithuania.svg',
+    },
+    {
+        id: 'img-2-1',
+        img: 'img/002-ukraine.svg',
+    },
+    {
+        id: 'img-3-1',
+        img: 'img/003-belarus.svg',
+    },
+    {
+        id: 'img-4-1',
+        img: 'img/004-germany.svg',
+    },
+    {
+        id: 'img-5-1',
+        img: 'img/005-russia.svg',
+    },
+    {
+        id: 'img-6-1',
+        img: 'img/006-czech-republic.svg',
+    },
+    {
+        id: 'img-7-1',
+        img: 'img/007-slovakia.svg',
+    },
+    {
+        id: 'img-8-1',
+        img: 'img/008-poland.svg',
+    },
+    {
+        id: 'img-1-2',
+        img: 'img/001-lithuania.svg',
+    },
+    {
+        id: 'img-2-2',
+        img: 'img/002-ukraine.svg',
+    },
+    {
+        id: 'img-3-2',
+        img: 'img/003-belarus.svg',
+    },
+    {
+        id: 'img-4-2',
+        img: 'img/004-germany.svg',
+    },
+    {
+        id: 'img-5-2',
+        img: 'img/005-russia.svg',
+    },
+    {
+        id: 'img-6-2',
+        img: 'img/006-czech-republic.svg',
+    },
+    {
+        id: 'img-7-2',
+        img: 'img/007-slovakia.svg',
+    },
+    {
+        id: 'img-8-2',
+        img: 'img/008-poland.svg',
+    },
+];
+
+const openedCards = [];
+const matchedCards = [];
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = cards.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
+
+    return cards;
+}
+
+const shuffleButton = document.getElementById('shuffle-btn');
+shuffleButton.addEventListener('click', shuffle);
+
+const shuffledBoard = shuffle(cards);
+
+for (let i = 0; i < shuffledBoard.length; i++) {
+    const cardContainer = document.createElement('li');
+    const singleCard = document.createElement('div');
+    const figure = document.createElement('figure');
+    const secondFigure = document.createElement('figure');
+
+    list.appendChild(cardContainer);
+    cardContainer.appendChild(singleCard);
+    singleCard.appendChild(figure);
+    const figureFront = singleCard.insertBefore(secondFigure, figure);
+
+    const imgNode = document.createElement('img');
+
+    imgNode.classList.add('flag');
+    imgNode.setAttribute('src', shuffledBoard[i].img);
+    figure.appendChild(imgNode);
+
+
+    cardContainer.classList.add('card-container');
+    singleCard.classList.add('card');
+    figure.classList.add('back');
+    figureFront.classList.add('front');
+}
+
+
+function addEventListenersToCards() {
+    const cardContainer = document.querySelectorAll('.card-container');
+    for (let i = 0; i < cardContainer.length; i++) {
+        cardContainer[i].addEventListener('click', showClickedCard);
+    }
+}
+
+function showClickedCard(event) {
+    const clickedFigure = event.target;
+    const parentCard = clickedFigure.parentElement;
+    parentCard.classList.add('flipped');
+}
+
+
+initializeMemoryGame();
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
